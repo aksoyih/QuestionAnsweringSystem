ec80a3ec396a01533a77bf2c68e00a4140dcc78c
haluk <i.halukaksoy@gmail.com>
2021-07-15 01:07:08 +0300

    avatar upload and delete done

--- src/routers/user/user.js
+++ src/routers/user/user.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 
 const auth = require('../../middleware/auth/auth')
 const auth_teacher = require('../../middleware/auth/teacher')
@@ -7,6 +9,19 @@ const auth_admin = require('../../middleware/auth/admin')
 const User = require('../../models/users/base')
 
 const router = new express.Router()
+
+const upload = multer({
+    limits: {
+        fileSize: 3000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
 
 router.post('/users/login', async (req, res) => {
     try {
@@ -80,7 +95,6 @@ router.get('/profile/me', auth, async (req, res) => {
     res.send(userObject)
 })
 
-
 router.get('/profile/:username', auth_admin, async (req, res) => {
     
     const username = req.params.username;
@@ -100,6 +114,39 @@ router.get('/profile/:username', auth_admin, async (req, res) => {
         res.status(500).send(error)
     }
 
+})
+
+router.post('/profile/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+    req.user.avatar = buffer
+    await req.user.save()
+
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.get('/profile/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById({_id: req.params.id})
+
+        if(!user || !user.avatar){
+            throw new Erro()
+        }
+
+        res.set('Content-Type','image/png')
+        res.send(user.avatar)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+router.delete('/profile/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+
+    res.send()
 })
 
 module.exports = router
