4ae584156c82f445e46f6c6e05b9756ebbe14935
haluk <i.halukaksoy@gmail.com>
2021-07-14 19:47:41 +0300

    delete courses and classes

--- src/routers/utils/class.js
+++ src/routers/utils/class.js
@@ -26,5 +26,43 @@ router.get('/classes', auth, async (req, res) => {
     }
 })
 
+router.patch('/classes/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['class_name']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send("Invalid updates!")
+    }
+
+    try {
+        const savedClass = await Class.findOne({ _id: req.params.id})
+
+        if (!savedClass) {
+            throw new Error("Could not find class")
+        }
+
+        updates.forEach((update) => savedClass[update] = req.body[update])
+        await savedClass.save()
+        res.send(savedClass)
+    } catch (e) {
+        res.status(400).send({error: e.message})
+    }
+})
+
+router.delete('/classes/:id', auth, async (req, res) => {
+    try {
+        const savedClass = await Class.findOneAndDelete({ _id: req.params.id})
+
+        if (!savedClass) {
+            res.status(404).send({error: 'Could not find class'})
+        }
+
+        res.send(savedClass)
+    } catch (e) {
+        res.status(500).send({error: e.message})
+    }
+})
+
 
 module.exports = router
--- src/routers/utils/course.js
+++ src/routers/utils/course.js
@@ -26,5 +26,46 @@ router.get('/courses', auth, async (req, res) => {
     }
 })
 
+router.patch('/courses/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['course_name']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+
+    if (!isValidOperation) {
+        return res.status(400).send("Invalid updates!")
+    }
+
+    try {
+        const course = await Course.findOne({ _id: req.params.id})
+
+        if (!course) {
+            throw new Error("Could not find course")
+        }
+
+        updates.forEach((update) => course[update] = req.body[update])
+        await course.save()
+        res.send(course)
+
+    } catch (e) {
+        return console.log(e)
+        res.status(400).send({error: e.message})
+    }
+})
+
+router.delete('/courses/:id', auth, async (req, res) => {
+    try {
+        const course = await Course.findOneAndDelete({ _id: req.params.id})
+
+        if (!course) {
+            res.status(404).send({error: 'Could not find course'})
+        }
+
+        res.send(course)
+    } catch (e) {
+        res.status(500).send({error: e.message})
+    }
+})
+
 
 module.exports = router
